1fa050717d5d2b1de83c4d9b4f308baae83c999d
Judongsung <57513715+Judongsung@users.noreply.github.com>
2022-05-25 04:06:51 +0900

    [Gold V] Title: 인구 이동, Time: 968 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -4,10 +4,11 @@ sys.setrecursionlimit(10**6)
 def init_board(n, data):
     return [[data for _ in range(n)] for _ in range(n)]
 
-def dfs(cmap, checked, left, right, r, c):
+def dfs(cmap, locked, checked, left, right, r, c):
     if checked[r][c]:
         return []
     checked[r][c] = True
+    locked[r][c] = True
     
     result = [(r, c)]
     r_dirs = [-1, 0, 0, 1]
@@ -18,7 +19,7 @@ def dfs(cmap, checked, left, right, r, c):
         sc = c+c_dir
         if 0 <= sr < n and 0 <= sc < n and not checked[sr][sc]\
                 and left <= abs(cmap[r][c]-cmap[sr][sc]) <= right:
-            result += dfs(cmap, checked, left, right, sr, sc)
+            result += dfs(cmap, locked, checked, left, right, sr, sc)
     
     return result
     
@@ -26,6 +27,7 @@ def dfs(cmap, checked, left, right, r, c):
 def solution(n, left, right, countries):
     cmap = [row.copy() for row in countries]
     day_count = 0
+    locked = init_board(n, False)
     r_dirs = [-1, 0, 0, 1]
     c_dirs = [0, -1, 1, 0]
     
@@ -35,7 +37,9 @@ def solution(n, left, right, countries):
         
         for i in range(n):
             for j in range(n):
-                union = dfs(cmap, checked, left, right, i, j)
+                union = []
+                if not locked[i][j]:
+                    union = dfs(cmap, locked, checked, left, right, i, j)
                 if len(union) > 1:
                     unions.append(union)
         
@@ -45,6 +49,7 @@ def solution(n, left, right, countries):
             popul_avg = sum([cmap[i][j] for i, j in union])//len(union)
             for i, j in union:
                 cmap[i][j] = popul_avg
+                locked[i][j] = False
         
         day_count += 1
     
