b833e87995fb706d3cfd3723a755908366f5174b
Judongsung <57513715+Judongsung@users.noreply.github.com>
2022-06-06 06:44:51 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 380 ms, Memory: 158212 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -10,18 +10,17 @@ def dfs(tree, parent_map, parent, cur):
         parent_map[conn] = cur
         dfs(tree, parent_map, cur, conn)
                 
-def solution(n, relations):
+def solution(n, tree):
     parent_map = [None]*(n+1)
-    tree = [None]+[[] for _ in range(n)]
-    for a, b in relations:
-        tree[a].append(b)
-        tree[b].append(a)
-    
     dfs(tree, parent_map, None, 1)
     return parent_map[2:]
     
 n = int(stdin.readline())
-relations = [list(map(int, stdin.readline().split())) for _ in range(n-1)]
-result = solution(n, relations)
+tree = [None]+[[] for _ in range(n)]
+for _ in range(n-1):
+    a, b = map(int, stdin.readline().split())
+    tree[a].append(b)
+    tree[b].append(a)
+result = solution(n, tree)
 for el in result:
     print(el)
