0c49523a26c776e999a1e50be4e6c272810166ea
Judongsung <57513715+Judongsung@users.noreply.github.com>
2022-06-16 23:10:16 +0900

    [Gold II] Title: 트리의 순회, Time: 456 ms, Memory: 335336 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.py
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.py
@@ -4,17 +4,19 @@ from sys import setrecursionlimit
 setrecursionlimit(10**5)
 
 def solution(n, inorder, postorder):
-    rec(inorder, postorder, 0, n-1, 0, n-1)
-    return
+    index_map = [0]*(n+1)
+    for i in range(n):
+        index_map[inorder[i]] = i
+    rec(index_map, inorder, postorder, 0, n-1, 0, n-1)
     
-def rec(inorder, postorder, in_start, in_end, post_start, post_end):
+def rec(index_map, inorder, postorder, in_start, in_end, post_start, post_end):
     if in_start == in_end:
         print(inorder[in_start], end=' ')
         return
     elif in_start > in_end:
         return
     root = postorder[post_end]
-    inorder_root_idx = inorder.index(root)
+    inorder_root_idx = index_map[root]
     
     in_left_start = in_start
     in_left_end = inorder_root_idx-1
@@ -27,11 +29,11 @@ def rec(inorder, postorder, in_start, in_end, post_start, post_end):
     post_right_end = post_end-1
     
     print(root, end=' ')
-    rec(inorder, postorder, in_left_start, in_left_end, post_left_start, post_left_end)
-    rec(inorder, postorder, in_right_start, in_right_end, post_right_start, post_right_end)
+    rec(index_map, inorder, postorder, in_left_start, in_left_end, post_left_start, post_left_end)
+    rec(index_map, inorder, postorder, in_right_start, in_right_end, post_right_start, post_right_end)
     return
 
 n = int(stdin.readline())
-inorder = stdin.readline().split()
-postorder = stdin.readline().split()
+inorder = list(map(int, stdin.readline().split()))
+postorder = list(map(int, stdin.readline().split()))
 preorder = solution(n, inorder, postorder)
