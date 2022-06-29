027cdc94742081a4f8a7d7e755ad4fe21808b9ee
Judongsung <57513715+Judongsung@users.noreply.github.com>
2022-06-29 12:26:23 +0900

    [Gold IV] Title: 이분 그래프, Time: 4320 ms, Memory: 63624 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -0,0 +1,41 @@
+from sys import stdin
+
+NOT_VISITED = -1
+
+def check_graph(graph, binary_map, root):
+    result = True
+    queue = [(root, False)]
+    flag = True
+    
+    while flag and queue:
+        num, state = queue.pop()
+        binary_map[num] = state
+        state = not state
+        for connected in graph[num]:
+            if binary_map[connected] != NOT_VISITED:
+                if binary_map[connected] != state:
+                    result = False
+                    flag = False
+                    break
+                continue
+            queue.append((connected, state))
+    
+    return result
+
+k = int(stdin.readline())
+for _ in range(k):
+    v, e = map(int, stdin.readline().split())
+    graph = [None]+[[] for __ in range(v)]
+    for __ in range(e):
+        u, v = map(int, stdin.readline().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    binary_map = [NOT_VISITED for __ in graph]
+    binary_map[0] = 0
+    result = 'YES'
+    
+    while NOT_VISITED in binary_map:
+        if not check_graph(graph, binary_map, binary_map.index(NOT_VISITED)):
+            result = 'NO'
+            break
+    print(result)
