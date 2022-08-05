b1cb3a26cf0160e1bd184158ccbb5fc26b7c6b9e
Judongsung <57513715+Judongsung@users.noreply.github.com>
2022-08-05 10:01:27 +0900

    [Gold IV] Title: 알파벳, Time: 6348 ms, Memory: 116192 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,23 +1,28 @@
 from sys import stdin
 
-def dfs(board, row, col, visited=[False for _ in range(26)], cur_r=0, cur_c=0):
-    cur_alpha = board[cur_r][cur_c]
-    alpha_idx = ord(cur_alpha)-65
-    if visited[alpha_idx]:
-        return 0
+def dfs(board, row, col):
     dirs = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-    cur_visited = visited.copy()
-    cur_visited[alpha_idx] = True
-    max_count = 0
+    max_count = 1
+    queue = set()
+    queue.add((0, 0, board[0][0]))
     
-    for r_dir, c_dir in dirs:
-        next_r = cur_r+r_dir
-        next_c = cur_c+c_dir
-        if 0 <= next_r < row and 0 <= next_c < col:
-            temp = dfs(board, row, col, cur_visited, next_r, next_c)
-            max_count = max(temp, max_count)
+    while queue:
+        r, c, route = queue.pop()
+        
+        flag = False
+        for r_dir, c_dir in dirs:
+            next_r = r+r_dir
+            next_c = c+c_dir
+            if 0 <= next_r < row and 0 <= next_c < col and board[next_r][next_c] not in route:
+                queue.add((next_r, next_c, route+board[next_r][next_c]))
+                flag = True
+        
+        if not flag:
+            max_count = max(len(route), max_count)
+            if max_count == 26:
+                break
     
-    return max_count+1
+    return max_count
 
 
 row, col = map(int, stdin.readline().split())
