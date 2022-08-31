2b0e37adae0fdc8cd5f53bfc7c2f692939d9dc50
Judongsung <57513715+Judongsung@users.noreply.github.com>
2022-09-01 08:25:52 +0900

    [Gold IV] Title: 트리의 지름, Time: 96 ms, Memory: 42792 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -0,0 +1,31 @@
+from sys import stdin
+from sys import setrecursionlimit
+
+setrecursionlimit(10**6)
+
+def find_max_radius(cur, prev_length):
+    global max_radius
+    max_length = 0
+    second_max_length = 0
+    
+    for connect, weight in graph[cur]:
+        length = find_max_radius(connect, prev_length)+weight
+        if length > max_length:
+            second_max_length = max_length
+            max_length = length
+        elif length > second_max_length:
+            second_max_length = length
+            
+    radius = max_length+max(second_max_length, prev_length)
+    max_radius = max(max_radius, radius)
+    return max_length
+
+n = int(stdin.readline())
+graph = [[] for _ in range(n+1)]
+max_radius = 0
+for _ in range(n-1):
+    parent, child, weight = map(int, stdin.readline().split())
+    graph[parent].append((child, weight))
+    
+find_max_radius(1, 0)
+print(max_radius)
