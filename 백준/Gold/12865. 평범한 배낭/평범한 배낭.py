f142a970b0f23be22c10b94b6c9111d873bf2342
Judongsung <57513715+Judongsung@users.noreply.github.com>
2022-09-03 07:21:39 +0900

    [Gold V] Title: 평범한 배낭, Time: 5860 ms, Memory: 281028 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -7,12 +7,13 @@ for i in range(n):
     weight, value = map(int, stdin.readline().split())
     weights[i] = weight
     values[i] = value
+min_weight = min(weights)
 memo = [[0 for __ in range(k+1)] for _ in range(n+1)]
 
 for i in range(1, n+1):
     weight = weights[i-1]
     value = values[i-1]
-    for j in range(1, k+1):
+    for j in range(min_weight, k+1):
         if weight > j:
             memo[i][j] = memo[i-1][j]
         else:
