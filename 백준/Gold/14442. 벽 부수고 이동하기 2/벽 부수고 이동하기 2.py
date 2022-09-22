07a468fc80483a051d40f6e7a7630ebcfe39a9c3
Judongsung <57513715+Judongsung@users.noreply.github.com>
2022-09-23 00:37:58 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 2516 ms, Memory: 248020 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -1,17 +1,16 @@
 from sys import stdin
 from collections import deque
 
-INF = 10**8
 EMPTY = '0'
 WALL = '1'
 DIRS = [(1, 0), (0, 1), (0, -1), (-1, 0)]
 
 def find_min_distance():
-    min_distance = INF
+    global visited
     goal = (n-1, m-1)
     queue = deque([(0, 0, k, 0)])
-    visited = [[[False for ___ in range(k+1)] for __ in range(m)] for _ in range(n)]
-    visited[0][0][k] = True
+    visited = [[-1 for __ in range(m)] for _ in range(n)]
+    visited[0][0] = k
     
     while queue:
         r, c, breakable, distance = queue.popleft()
@@ -23,12 +22,12 @@ def find_min_distance():
             next_r = r+r_dir
             next_c = c+c_dir
             if 0 <= next_r < n and 0 <= next_c < m:
-                if board[next_r][next_c] == EMPTY and not visited[next_r][next_c][breakable]:
+                if board[next_r][next_c] == EMPTY and breakable > visited[next_r][next_c]:
                     queue.append((next_r, next_c, breakable, distance+1))
-                    visited[next_r][next_c][breakable] = True
-                elif breakable and not visited[next_r][next_c][breakable-1]:
+                    visited[next_r][next_c] = breakable
+                elif breakable and breakable-1 > visited[next_r][next_c]:
                     queue.append((next_r, next_c, breakable-1, distance+1))
-                    visited[next_r][next_c][breakable-1] = True
+                    visited[next_r][next_c] = breakable-1
     else:
         min_distance = -1
     
