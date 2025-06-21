d5526358bdca5a13d463cadc9f5c8586bebad8db
Judongsung <57513715+Judongsung@users.noreply.github.com>
2025-06-22 01:05:22 +0900

    [Silver III] Title: 배열 복원하기, Time: 132 ms, Memory: 39788 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -0,0 +1,18 @@
+from sys import stdin
+
+
+def restore_array(height: int, weight: int, x: int, y: int, array: list) -> list:
+    restored = [[array[h][w] for w in range(weight)] for h in range(height)]
+    
+    for h in range(x, height):
+        for w in range(y, weight):
+            restored[h][w] -= restored[h-x][w-y]
+
+    return restored
+
+h, w, x, y = map(int, stdin.readline().rstrip().split())
+new_array = [list(map(int, stdin.readline().rstrip().split())) for _ in range(h+x)]
+
+result = restore_array(h, w, x, y, new_array)
+for line in result:
+    print(*line)
