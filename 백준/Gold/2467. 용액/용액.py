9eeec1eaf97e7f627ef2d4d7770b6f6823715bef
Judongsung <57513715+Judongsung@users.noreply.github.com>
2025-07-20 07:35:45 +0900

    [Gold V] Title: 용액, Time: 68 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,43 +1,28 @@
 from sys import stdin
-
-
-def bisect_nearest(lst: list, start_left: int, start_right: int, target: int) -> int:
-    left = start_left
-    right = start_right
-    while left <= right:
-        mid = (left+right)//2
-        if lst[mid] > target:
-            right = mid-1
-        elif lst[mid] < target:
-            left = mid+1
-        else:
-            return mid
-
-    candidates = []
-    if left <= start_right:
-        candidates.append(left)
-    if right >= start_left:
-        candidates.append(right)
-    
-    return min(candidates, key=lambda x:abs(lst[x]-target))
         
 
 def find_zero_nearest_sum(features: list) -> tuple:
     nearest_left = 0
-    nearest_right = 1
+    nearest_right = len(features)-1
     nearest_sum = float('inf')
+    left = nearest_left
+    right = nearest_right
 
-    for right in range(1, len(features)):
-        left = bisect_nearest(features, 0, right-1, -features[right])
-        left_feature = features[left]
-        right_feature = features[right]
-        abs_sum = abs(left_feature+right_feature)
+    while left < right:
+        cur_sum = features[left]+features[right]
+        abs_sum = abs(cur_sum)
         if abs_sum < nearest_sum:
             nearest_sum = abs_sum
-            nearest_left = left_feature
-            nearest_right = right_feature
+            nearest_left = left
+            nearest_right = right
 
-    return nearest_left, nearest_right
+        if cur_sum > 0:
+            right -= 1
+        else:
+            left += 1
+
+    return features[nearest_left], features[nearest_right]
+
 
 n = int(stdin.readline().rstrip())
 features = list(map(int, stdin.readline().rstrip().split()))
