5f9fc54fa39c949c527d419a993bc21672915459
Judongsung <57513715+Judongsung@users.noreply.github.com>
2025-11-03 20:44:50 +0900

    [Silver III] Title: 군꺾문자열, Time: 60 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/34620. 군꺾문자열/군꺾문자열.py
@@ -0,0 +1,34 @@
+from sys import stdin
+from collections import deque
+
+
+ADD_ONE= 'G'
+DIV_TWO= 'K'
+IMPOSSIBLE = '-1'
+
+def is_power_of_two(n: int) -> bool:
+    if b & (b-1) == 0:
+        return True
+    return False
+
+def find_shortest_path(nume: int, deno: int) -> list[list[int]]:
+    if not is_power_of_two(deno):
+        return IMPOSSIBLE
+        
+    path = []
+
+    while (nume, deno) != (0, 1):
+        if nume >= deno:
+            k, nume = divmod(nume, deno)
+            path += [ADD_ONE]*k
+            
+        else:
+            path.append(DIV_TWO)
+            deno //= 2
+
+    return ''.join(reversed(path))
+    
+a, b = map(int, stdin.readline().rstrip().split())
+result = find_shortest_path(a, b)
+
+print(result)
