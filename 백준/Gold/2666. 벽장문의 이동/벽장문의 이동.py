4b219484ca4d0422d2943dad5f88911e3710763c
Judongsung <57513715+Judongsung@users.noreply.github.com>
2025-12-06 20:49:27 +0900

    [Gold V] Title: 벽장문의 이동, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.py
+++ 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.py
@@ -2,32 +2,31 @@ from sys import stdin
 
 
 def find_min_move(left: int, right: int, goals: list[int]) -> int:
-    def find_next_moves(left: int, right: int, goal: int, prev_move: int) -> dict[tuple[int], int]:
-        result = {}
-        
-        if 0 <= goal < right:
-            result[(goal, right)] = prev_move+abs(left-goal)
-            
-        if left < goal <= n:
-            result[(left, goal)] = prev_move+abs(right-goal)
-
-        return result
-
-    dp = find_next_moves(left, right, goals[0], 0)
+    dp = {(left, right): 0}
     
-    for goal in goals[1:]:
+    for goal in goals:
         next_dp = {}
         for key, val in dp.items():
             l, r = key
-            next_moves = find_next_moves(l, r, goal, val)
 
-            for next_key, next_val in next_moves.items():
+            if goal < r:
+                next_key = tuple(sorted([goal, r]))
+                next_val = val + abs(l-goal)
+                
                 if next_key not in next_dp:
                     next_dp[next_key] = next_val
-                    continue
-                    
-                next_dp[next_key] = min(next_val, next_dp[next_key])
+                else:
+                    next_dp[next_key] = min(next_val, next_dp[next_key])
 
+            if goal > l:
+                next_key = tuple(sorted([l, goal]))
+                next_val = val + abs(r-goal)
+                
+                if next_key not in next_dp:
+                    next_dp[next_key] = next_val
+                else:
+                    next_dp[next_key] = min(next_val, next_dp[next_key])
+            
         dp = next_dp
         
     return min(dp.values())
