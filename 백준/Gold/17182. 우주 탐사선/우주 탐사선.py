3eb2cf669344e2290b702e210a0d0ca03cd0cb79
Judongsung <57513715+Judongsung@users.noreply.github.com>
2025-12-21 19:38:00 +0900

    [Gold III] Title: 우주 탐사선, Time: 68 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/17182. 우주 탐사선/우주 탐사선.py
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.py
@@ -16,25 +16,25 @@ def find_min_paths(n: int, matrix: list[list[int]]) -> list[list[int]]:
     return result
 
 def find_min_time(n: int, k: int, paths: list[list[int]]) -> int:
-    dp = [[float('inf')]*n for _ in range(to_bit(n))] # dp[visited][position]
-    dp[to_bit(k)][k] = 0
-    q = deque()
-    q.append((to_bit(k), k))
+    dp = [[-1]*n for _ in range(to_bit(n))] # dp[visited][position]
 
-    while q:
-        visited, pos = q.popleft()
+    def dfs(visited: int, pos: int) -> int:
+        if visited == to_bit(n)-1:
+            return 0
+
+        if dp[visited][pos] != -1:
+            return dp[visited][pos]
+
+        min_time = float('inf')
 
         for np in range(n):
-            b_np = to_bit(np)
-            if visited & b_np:
-                continue
-                
-            next_visited = visited|b_np
-            
-            dp[next_visited][np] = min(dp[next_visited][np], dp[visited][pos]+paths[pos][np])
-            q.append((next_visited, np))
+            if not (visited & to_bit(np)):
+                min_time = min(min_time, paths[pos][np]+dfs(visited|to_bit(np), np))
+        
+        dp[visited][pos] = min_time
+        return min_time
 
-    return min(dp[to_bit(n)-1])
+    return dfs(to_bit(k), k)
 
 n, k = map(int, stdin.readline().rstrip().split())
 matrix = []
