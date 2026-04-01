a666955a19066933bc0b78860855f14f8b935b99
Judongsung <57513715+Judongsung@users.noreply.github.com>
2026-04-01 14:10:10 +0900

    [Gold II] Title: 지역, Time: 56 ms, Memory: 6640 KB -BaekjoonHub

--- 백준/Gold/1304. 지역/지역.cs
+++ 백준/Gold/1304. 지역/지역.cs
@@ -5,14 +5,7 @@ string[] input = reader.ReadLine().Split();
 int n = int.Parse(input[0]);
 int m = int.Parse(input[1]);
 
-List<int>[] graph = new List<int>[n];
-
-for (int i=0;i<n;i++)
-{
-    graph[i] = new List<int>();
-
-    if (i < n-1) graph[i].Add(i+1);
-}
+List<(int s, int e)> backwardEdges = new List<(int s, int e)>();
 
 for (int i=0;i<m;i++)
 {
@@ -20,7 +13,7 @@ for (int i=0;i<m;i++)
     int s = int.Parse(input[0]) - 1; // 1-base to 0-base
     int e = int.Parse(input[1]) - 1;
     
-    graph[s].Add(e);
+    if (s > e) backwardEdges.Add((s, e));
 }
 
 List<int> divisors = GetDivisors(n);
@@ -30,9 +23,9 @@ foreach (int d in divisors)
 {
     bool isFail = false;
 
-    for (int start=n-d;start>0;start-=d)
+    foreach (var edge in backwardEdges)
     {
-        if (HasLowerWay(start))
+        if (edge.s / d != edge.e / d)
         {
             isFail = true;
             break;
@@ -64,28 +57,4 @@ List<int> GetDivisors(int num)
     }
 
     return divisors;
-}
-
-bool HasLowerWay(int start)
-{
-    Queue<int> queue = new Queue<int>();
-    bool[] visited = new bool[n];
-    queue.Enqueue(start);
-    visited[start] = true;
-
-    while (queue.Count != 0)
-    {
-        int cur = queue.Dequeue();
-
-        foreach (int next in graph[cur])
-        {
-            if (visited[next]) continue;
-            if (next < start) return true;
-
-            queue.Enqueue(next);
-            visited[next] = true;
-        }
-    }
-
-    return false;
 }
