14e89c093da7785ca0381d8dffe729163d9b9cec
Judongsung <57513715+Judongsung@users.noreply.github.com>
2026-04-02 13:40:41 +0900

    [Gold IV] Title: 육각수, Time: 1884 ms, Memory: 9736 KB -BaekjoonHub

---
+++ 백준/Gold/1229. 육각수/육각수.cs
@@ -0,0 +1,33 @@
+using var reader = new StreamReader(Console.OpenStandardInput());
+using var writer = new StreamWriter(Console.OpenStandardOutput());
+
+int n = int.Parse(reader.ReadLine());
+List<int> hexs = new List<int>();
+int[] dp = new int[n+1];
+Array.Fill(dp, int.MaxValue);
+dp[0] = 0;
+
+int cur = 0;
+int i = 1;
+while (true)
+{
+    cur = i * (i * 2 - 1);
+
+    if (cur > n) break;
+
+    hexs.Add(cur);
+    dp[cur] = 1;
+    i++;
+}
+
+for (int num=1;num<=n;num++)
+{
+    foreach (int hex in hexs)
+    {
+        if (hex > num) break;
+
+        dp[num] = Math.Min(dp[num], dp[num - hex] + 1);
+    }
+}
+
+writer.WriteLine(dp[n]);
